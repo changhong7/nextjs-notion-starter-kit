00f2ae5f2c5a9ecb107e9910af595c045e350d24
changhong7 <67247163+changhong7@users.noreply.github.com>
2024-01-07 20:10:14 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'cada67aff367468581680692915d5cf8',
+  rootNotionPageId: 'e6af543588484cd583f767c3b5268391',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: '7777',
-  domain: 'https://miniature-platypus-b51.notion.site/cada67aff367468581680692915d5cf8?pvs=4',
+  domain: 'https://miniature-platypus-b51.notion.site/e6af543588484cd583f767c3b5268391',
   author: '888888',
 
   // open graph metadata (optional)
