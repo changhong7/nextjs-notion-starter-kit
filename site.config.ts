52b43ec22fb714dc12b0a0f2dcd959bb81e5b7e4
changhong7 <67247163+changhong7@users.noreply.github.com>
2024-01-07 19:54:59 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'cada67aff367468581680692915d5cf8',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '7777',
+  domain: 'https://miniature-platypus-b51.notion.site/cada67aff367468581680692915d5cf8?pvs=4',
+  author: '888888',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
